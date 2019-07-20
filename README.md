75f648eed56e7237f045268c572390613a6bc5ab
Dau Lam <southphillysouth@gmail.com>
2019-07-19 21:52:25 -0400

    Refactored

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # csv-enrollment-batch
 simple program to process csv enrollment files 
 
+# Required
+java 8+
+
 # Usage
 csv files are in the project root directory. 
 1. Using your favorite IDE, open up the project.
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -9,10 +12,12 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, SortedSet> mapOfFiles = processCSVEnrollments("input.csv");
-
-        print(mapOfFiles);
-
+        List listOfEnrollments = readFile("input.csv");    // reads the csv file
+        Map mapOfFiles = processCSVEnrollments(listOfEnrollments);  // separate by insurance company
+                                                                    // and sort the enrollment by names
+                                                                    // and finally if duplicates,
+                                                                    // take record greatest version number
+        print(mapOfFiles);                                          // print the parsed data
     }
 
     /**
@@ -31,31 +36,62 @@ public class Main {
     }
 
     /**
+     * Method to read and parse a csv file and return a list of enrollment records
+     *
+     * @param filename
+     * @return list of enrollment records
+     */
+    public static List<Enrollment> readFile(String filename)
+    {
+        String line = "";
+        String cvsSplitBy = ",";
+        List<Enrollment> listOfRecords = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] record = line.split(cvsSplitBy);
+                Enrollment enrollment = new Enrollment(record[0], record[1], record[2], Integer.parseInt(record[3]), record[4]);
+                listOfRecords.add(enrollment);
+            }
+
+        } catch (IOException e) {
+            System.out.printf("Exception while reading file [%s]\n", filename);
+            throw e;
+        } finally
+        {
+            return listOfRecords;
+        }
+    }
+
+    /**
      * Process the csv file into a map
      *
-     * @param fileName the csv file path and name
+     * @param listOfEnrollments list of enrollments records
      * @return map of insurance company files
      */
-    private static Map<String, SortedSet> processCSVEnrollments(String fileName) {
-        Map<String, SortedSet> mapOfFiles = new HashMap<>(); // map with insurance names as key, and the collection of enrollment records as value
-
-        List<Enrollment> listOfEnrollments = EnrollmentCSVReader.readFile(fileName);
+    private static Map<String, SortedSet> processCSVEnrollments(List<Enrollment> listOfEnrollments) {
+        Map<String, SortedSet> mapOfFiles = new HashMap<>();    // map with insurance names as key,
+                                                                // and the collection of enrollment records as value
 
         for (Enrollment enrollment : listOfEnrollments)
         {
             SortedSet<Enrollment> file = mapOfFiles.get(enrollment.getInsuranceCompany());
             if (file != null)
             {
-                boolean isSuccess = mapOfFiles.get(enrollment.getInsuranceCompany()).add(enrollment); // try to add the enrollment record
+                boolean isSuccess = file.add(enrollment); // try to add the enrollment record
                 if (isSuccess == false)
                 {
+                    // enrollment record already exists, duplicate record so we check the version
                     final String userId = enrollment.getUserId();
                     Enrollment existingEnrollment = file.stream()
                             .filter( e -> e.getUserId().equalsIgnoreCase(userId))
-                            .findFirst().orElse(null);
+                            .findFirst().orElse(null); // using userId as unique identifier, search and return the enrollment record
 
+                    // remove the duplicate and add the newer version
                     if (existingEnrollment.getVersion() < enrollment.getVersion() ) {
-                        // remove the duplicate and add the newer version
                         mapOfFiles.get(enrollment.getInsuranceCompany()).remove(existingEnrollment);
                         mapOfFiles.get(enrollment.getInsuranceCompany()).add(enrollment);
                     }
@@ -63,6 +99,8 @@ public class Main {
             }
             else
             {
+                // new insurance company encountered
+                // create a set and add the enrollment
                 file =  new TreeSet<>();
                 file.add(enrollment);
                 mapOfFiles.put(enrollment.getInsuranceCompany(), file);
